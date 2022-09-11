344312af7374460f0d47c8383b219ed0c025c5e6
vigneshkumaresan <vickie1991@gmail.com>
2022-09-11 21:36:47 +0530

    Volatility from 1 day to 1 year

--- stockData/nifty50.sql
+++ stockData/nifty50.sql
@@ -18,7 +18,18 @@ select date(trading_date) trading_date ,
 
 
 
---Calculating Daily Volatility for 5 days, 10 days, 30 days, 90 days, 180 days, 365 days for Nifty 50
+
+--Calculating Daily Volatility based on 5 days, 10 days, 30 days, 90 days, 180 days, 365 days dataset for Nifty 50
+--How to interpret the output:
+	--For Daily Volatility:
+			--1. Identify the volatility for 365 days.
+				--Compare it against the volatility for 90 days and 180 days. 
+				--If the 90/180 days volatility is larger than 365 days volatility, in the last 3,6 months - The stock is facing higher volatility.
+				--If the 90/180 days volatility is lesser than 365 days volatility, in the last 3,6 months - The stock is facing lower volatility.
+			--2. Identify the volatility for 365 days.
+				--Compare it against the volatility for 5 days and 10 days and 30 days. 
+				--If the 5/10/30 days volatility is larger than 90/180 but lesser than 365 - Then in the recent 1 month, the stock once again started showing high volatility.
+				--If the 5/10/30 days volatility is lesser than 90/180 and lesser than 365 - Then in the recent 1 month, the stock is showing lower volatility.
 with cte as(
 				select max(trading_date)-5 trading_date5, 
 				max(trading_date)-10 trading_date10, 
@@ -28,28 +39,45 @@ with cte as(
 				max(trading_date)-365 trading_date365
 				from nifty50_analysis
 			 )
-select 5 std_duration_in_days,stddev_samp(daily_volatility_percentage) 
+select 5 std_duration_in_days,stddev_samp(daily_volatility_percentage) daily_volatility, 
+stddev_samp(daily_volatility_percentage)*sqrt(52) weekly_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(12) monthly_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(252) annual_volatility
 from nifty50_analysis v,cte
 where v.trading_date>cte.trading_date5
 union
-select 10 std_duration_in_days,stddev_samp(daily_volatility_percentage) 
+select 10 std_duration_in_days,stddev_samp(daily_volatility_percentage) daily_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(52) weekly_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(12) monthly_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(252) annual_volatility
 from nifty50_analysis v,cte
 where v.trading_date>cte.trading_date10
 union
-select 30 std_duration_in_days,stddev_samp(daily_volatility_percentage) 
+select 30 std_duration_in_days,stddev_samp(daily_volatility_percentage) daily_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(52) weekly_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(12) monthly_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(252) annual_volatility
 from nifty50_analysis v,cte
 where v.trading_date>cte.trading_date30
 union
-select 90 std_duration_in_days,stddev_samp(daily_volatility_percentage) 
+select 90 std_duration_in_days,stddev_samp(daily_volatility_percentage) daily_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(52) weekly_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(12) monthly_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(252) annual_volatility
 from nifty50_analysis v,cte
 where v.trading_date>cte.trading_date90
 union
-select 180 std_duration_in_days,stddev_samp(daily_volatility_percentage) 
+select 180 std_duration_in_days,stddev_samp(daily_volatility_percentage) daily_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(52) weekly_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(12) monthly_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(252) annual_volatility
 from nifty50_analysis v,cte
 where v.trading_date>cte.trading_date180
 union
-select 365 std_duration_in_days,stddev_samp(daily_volatility_percentage) 
+select 365 std_duration_in_days,stddev_samp(daily_volatility_percentage) daily_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(52) weekly_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(12) monthly_volatility,
+stddev_samp(daily_volatility_percentage)*sqrt(252) annual_volatility
 from nifty50_analysis v,cte
 where v.trading_date>cte.trading_date365
 order by std_duration_in_days;
-
